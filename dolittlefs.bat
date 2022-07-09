b9902876067763dd3c39dd18b32ecfcb6df9fbb3
owen <owen@owenduffy.net>
2022-07-09 18:38:42 +1000

    Revisions - misc.

--- dolittlefs.bat
+++ dolittlefs.bat
@@ -9,18 +9,22 @@ goto end
 
 :ESP8266
 echo Processing ESP8266
+rem generic (FS:64KB OTA:~470KB)
 set FSSIZE=0x10000
 set FSLOC=0x0eb000
+set FSBKSIZE=4096
 goto makeit
 
 :ESP32
+rem check these
 echo Processing ESP32
 set FSSIZE=0x30000
 set FSLOC=0x3d0000
+set FSBKSIZE=8192
 goto makeit
 
 :makeit
-mklittlefs -d 2 -c data -b 4096 -p 256 -s %FSSIZE% littlefs.%1.bin
+mklittlefs -d 2 -c data -b %FSBKSIZE% -p 256 -s %FSSIZE% littlefs.%1.bin
 
 rem exit /b
 rem goto end
